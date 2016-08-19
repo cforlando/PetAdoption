875d2396f20371700e077d0a453104789c59ab05
Keli'i Martin <kelii.d.martin@gmail.com>
2016-08-19 19:07:16 -0400

    Initial changes

--- PetAdoption-iOS/PetAdoption-iOS/PetListingViewController.swift
+++ PetAdoption-iOS/PetAdoption-iOS/PetListingViewController.swift
@@ -8,92 +8,87 @@
 
 import UIKit
 
-class PetListingViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-	static let SEGUE_TO_PET_DETAILS_ID = "segueToPetDetails";
-	
-	
+class PetListingViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource
+{
+	static let SEGUE_TO_PET_DETAILS_ID = "segueToPetDetails"
+
 	@IBOutlet weak var collectionView: UICollectionView!
 	
-	var petData : [Pet] = [];
-	let collectionViewCellId = "normal-cell";
+	var petData : [Pet] = []
+	let collectionViewCellId = "normal-cell"
 	var viewControllerTitle: String = "Home"
 	
-	required init?(coder aDecoder: NSCoder) {
+	required init?(coder aDecoder: NSCoder)
+    {
 		super.init(coder: aDecoder);
-		NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(PetListingViewController.showPetDetails(_:)), name: PetImageCollectionViewCell.DID_TAP_ON_PET_CELL_NOTIFICATION, object: nil);
-
+		NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(PetListingViewController.showPetDetails(_:)), name: PetImageCollectionViewCell.DID_TAP_ON_PET_CELL_NOTIFICATION, object: nil)
 	}
 	
-	
-	override func viewDidLoad() {
+	override func viewDidLoad()
+    {
 		super.viewDidLoad()
-		self.navigationItem.title = NSLocalizedString("Town Of Lady Lake", comment: "");
-		self.collectionView.delegate = self;
-		self.collectionView.dataSource = self;
-		self.collectionView.backgroundColor = UIColor.groupTableViewBackgroundColor();
-		self.collectionView.collectionViewLayout = CustomHomeCollectionViewFlowLayout();
-		self.collectionView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0);
+		self.navigationItem.title = NSLocalizedString("Town Of Lady Lake", comment: "")
+		self.collectionView.delegate = self
+		self.collectionView.dataSource = self
+		self.collectionView.backgroundColor = UIColor.groupTableViewBackgroundColor()
+		self.collectionView.collectionViewLayout = CustomHomeCollectionViewFlowLayout()
+		self.collectionView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
 		
-		let normalCellView = UINib(nibName: "PetImageCollectionViewCell", bundle: nil);
-		self.collectionView.registerNib(normalCellView, forCellWithReuseIdentifier: collectionViewCellId);
-		
-		
-		
+		let normalCellView = UINib(nibName: "PetImageCollectionViewCell", bundle: nil)
+		self.collectionView.registerNib(normalCellView, forCellWithReuseIdentifier: collectionViewCellId)
+
 		//Load some data (fake data for now)
-		let petService = FindPetsService();
+		let petService = FindPetsService()
 		petService.execute({ (result) -> Void in
 			if (result.code == .Success) {
-				self.petData = result.petsFound;
-				self.collectionView.reloadData();
+				self.petData = result.petsFound
+				self.collectionView.reloadData()
 			}
 			else {
 				//TODO: Handle error case
 			}
-		});
+		})
 	}
-	
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-	
 	
 	// MARK: - NSNotification listeners
-	func showPetDetails(notification : NSNotification) {
+	func showPetDetails(notification : NSNotification)
+    {
 		let visible = (self.isViewLoaded() && self.view.window != nil)
-		if visible {
-			let pet = notification.object as! Pet;
-			self.performSegueWithIdentifier(PetListingViewController.SEGUE_TO_PET_DETAILS_ID, sender: pet);
+		if visible
+        {
+			let pet = notification.object as! Pet
+			self.performSegueWithIdentifier(PetListingViewController.SEGUE_TO_PET_DETAILS_ID, sender: pet)
 		}
 	}
 	
 	// MARK: - Navigation delegate
-	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-		if (segue.identifier == PetListingViewController.SEGUE_TO_PET_DETAILS_ID) {
-			let detailsVC = segue.destinationViewController as! PetListingDetailViewController;
-			detailsVC.pet = sender as! Pet;
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
+    {
+		if (segue.identifier == PetListingViewController.SEGUE_TO_PET_DETAILS_ID)
+        {
+			let detailsVC = segue.destinationViewController as! PetListingDetailViewController
+			detailsVC.pet = sender as! Pet
 		}
 	}
 	
-	
 	// MARK: - UICollectionViewDataSource and Delegate
-	func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-		return 1;
+	func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int
+    {
+		return 1
 	}
 	
-	func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-		return self.petData.count;
+	func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+		return self.petData.count
 	}
 	
-	func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-		let cell = collectionView.dequeueReusableCellWithReuseIdentifier(collectionViewCellId, forIndexPath: indexPath) as? PetImageCollectionViewCell;
-	
-	
+	func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell
+    {
+		let cell = collectionView.dequeueReusableCellWithReuseIdentifier(collectionViewCellId, forIndexPath: indexPath) as? PetImageCollectionViewCell
+
+		let pet = petData[indexPath.row]
+		cell!.updateWithPet(pet)
 		
-		let pet = petData[indexPath.row];
-		cell!.updateWithPet(pet);
-		
-		return cell!;
+		return cell!
 	}
-	
 }
---
+++ PetAdoption-iOS/PetAdoption-iOS/Views/PetCell.swift
@@ -0,0 +1,16 @@
+//
+//  PetCell.swift
+//  PetAdoption-iOS
+//
+//  Created by Keli'i Martin on 8/19/16.
+//  Copyright © 2016 Code For Orlando. All rights reserved.
+//
+
+import UIKit
+
+class PetCell: UICollectionViewCell
+{
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var petNameLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+}
