ef09dd40f3788d0e6c81c8d2ab8d16e070e2f2a5
Amir Fleminger <amirfl@yahoo.com>
2018-04-11 17:30:40 -0400

    add filter view controller class

---
+++ PetAdoption-iOS/PetAdoption-iOS/Controllers/filterViewControlelr.swift
@@ -0,0 +1,12 @@
+//
+//  filterViewControlelr.swift
+//  PetAdoption-iOS
+//
+//  Created by Amir Fleminger on 4/11/18.
+//  Copyright © 2018 Code For Orlando. All rights reserved.
+//
+
+import UIKit
+class filterViewController: UITableViewController {
+    
+}
